7c3f4946ca22c71666d9e5db6630a04dd18e044e
josuepalominoperez <oscar.palomino.perez@gmail.com>
2022-10-06 10:52:57 -0500

    refactoring guessingResult

--- guessingResult.js
+++ guessingResult.js
@@ -1,14 +1,8 @@
-//bucle hasta que adivine el resultado
-
-alert("Tendrás que ingresar el valor correcto de la suma");
-let valor= prompt("¿cuánto es 8 +4");
-
-while(valor!==12){
-    alert("suma incorrecta");
-    valor= prompt("¿cuánto es 8 +4");
-    if(valor==12){
-        alert("la suma es correcta");
-        break;
-    }
-    
-}
+alert("Ingrese valor correcto ");
+let valor;
+do{
+valor = prompt("8+4?");
+if(valor != 12) {
+    alert("Mal. Intente de nuevo ");}
+}while (valor != 12);
+alert("Correcto ");
