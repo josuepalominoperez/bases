2a70cfd3e30c82bbf8c1b39f535e7943fcd04063
josuepalominoperez <oscar.palomino.perez@gmail.com>
2022-10-22 23:43:01 -0500

    Accesing array element

---
+++ arrayAccesingElement.js
@@ -0,0 +1,7 @@
+const cars = ["Saab", "Volvo", "BMW"];
+cars[0] = "Opel";
+//Primera forma de accesar un elemento de un array es con el método at(index)
+document.write(cars.at(0)+"<br>");
+
+//El clásico llamado es usando los corchetes
+document.write(cars[1]);
