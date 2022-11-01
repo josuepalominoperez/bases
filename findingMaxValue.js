baf640cb1f10c6069299504cba1d6a268795b11f
josuepalominoperez <oscar.palomino.perez@gmail.com>
2022-11-01 00:46:50 -0500

    Find max value in another way

--- findingMaxValue.js
+++ findingMaxValue.js
@@ -1,4 +1,10 @@
+//Given an array , find max value
 let numbers=[1, 123, 500, 115, 44, 88];
 numbers.sort(function(a,b){return a - b});
 let number= numbers[numbers.length-1];
 document.write(number+"<br>");
+
+
+
+//or we can take and pass array as a parameter
+document.write(Math.max.apply(null, [1,2,3]));
