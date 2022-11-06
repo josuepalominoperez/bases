3c5fbff35bb56374cd7efeefed29183990f8274b
josuepalominoperez <oscar.palomino.perez@gmail.com>
2022-11-05 22:58:45 -0500

    callbacks

---
+++ Callbacks.js
@@ -0,0 +1,10 @@
+function myDisplayer(something) {
+    document.write(something);
+  }
+  
+  function myCalculator(num1, num2, myCallback) {
+    let sum = num1 + num2;
+    myCallback(sum);
+  }
+  
+  myCalculator(5, 6, myDisplayer);
