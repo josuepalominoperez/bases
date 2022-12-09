9e74fbcbe8a83856a6d34a62acc1e2f8c83ac818
josuepalominoperez <oscar.palomino.perez@gmail.com>
2022-12-08 20:34:02 -0500

    adding type

---
+++ type and interface/type.ts
@@ -0,0 +1,26 @@
+(()=>{
+
+    type Hero = {
+        name:string,
+        age?:number,
+        powers:string[],
+        getName?:() =>string;
+    }
+
+    let flash:Hero ={
+        name:'Dad',
+        age:24,
+        powers:['velocity' , 'strength']
+    }
+
+    let superman:Hero ={
+        name:'Grandfather',
+        age:34,
+        powers:['velocity'],
+        getName() {
+            return this.name;
+        },
+    }
+
+
+})()
